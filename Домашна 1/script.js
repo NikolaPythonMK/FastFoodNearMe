132876156f1cda8f76056907ba841c952995985c
NikolaPythonMK <76405057+NikolaPythonMK@users.noreply.github.com>
2022-11-13 17:30:11 +0100

    del

--- Домашна 1/script.js
+++
@@ -1,9 +0,0 @@
-const restaurants = require("./restaurants.js").restaurants.elements;
-const filters = require("./filters.js");
-const Pipe = require("./pipe.js").Pipe;
-let pipe = new Pipe();
-
-
-pipe.addFilter(filters.filterByDelivery);
-console.log(pipe.runFilters(restaurants));
-
