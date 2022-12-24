e74c6278c5606e9955078b7113b3418979fc4a91
NikolaPythonMk <nikola.python159@gmail.com>
2022-12-24 21:35:03 +0100

    removed bugs and added functionality

--- Domashna 3/frontEnd/scripts/script.js
+++ Domashna 3/frontEnd/scripts/script.js
@@ -66,6 +66,44 @@ function filterBy(property, value){
     }
 }
 
+function filterOpenedRestaurants(){
+    for (let i = 0; i < markers.length; i++) {
+        map.removeLayer(markers[i]);
+    }
+    markers.length = 0;
+    for(let i = 0; i < restaurants.length; i++){
+        if(!restaurants[i].tags.hasOwnProperty("opening_hours")){
+            continue;
+        }else{
+            let isOpen = false;
+            let now = new Date();
+            let restaurant_time = restaurants[i].tags["opening_hours"];
+            let parts = restaurant_time.split(";");
+
+            for(let i = 0; i < parts.length; i++){
+                isOpen = false;
+                let hours = parts[i].split(" ")[1];
+                let days = parts[i].split(" ")[0];
+                let beginnigHour = Number(hours[0] + hours[1]);
+                let endingHour = Number(hours[6] + hours[7]);
+                let openingDay = Number(days[0]);
+                let closingDay = Number(days[2]);
+                if(now.getHours() >= beginnigHour && now.getHours() <= endingHour && now.getDay() >= openingDay
+                && now.getDay() <= closingDay){
+                    isOpen = true;
+                    break;
+                }
+            }
+            if(isOpen){
+                addMarker(restaurants[i]);
+            }
+        }
+    }
+    if(markers.length == 0){
+        alert("Нема ресторани во моментов кој работат");
+    }
+}
+
 // Get a reference to the search input and search button
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
@@ -91,10 +129,6 @@ searchInput.addEventListener('keyup', function (event) {
     }
 });
 
-nearestEl.addEventListener('click', function(){
-
-});
-
 smokingEl.addEventListener('click', function(){
     filterBy("smoking", "isolated");
 });
@@ -108,7 +142,7 @@ outdoorseatingEl.addEventListener('click', function(){
 });
 
 openEl.addEventListener('click', function(){
-
+    filterOpenedRestaurants();
 });
 
 wifiEl.addEventListener('click', function(){
